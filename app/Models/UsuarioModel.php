2b9fde7417bb9d213228084fb1ad53bcda4e9897
Gilberto <gilbertonerigodoy@gmail.com>
2025-07-01 01:07:06 -0300

    Criando model e o controller de usuários

---
+++ app/Models/UsuarioModel.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UsuarioModel extends Model
+{
+    protected $table            = 'usuarios';    
+    protected $returnType       = 'object';
+    protected $useSoftDeletes   = true;    
+    protected $allowedFields    = [
+        'nome',
+        'email',
+        'password',
+        'reset_hash',
+       'reset_expira_em',
+       'imagem',
+       // Não colocaremos o campo ativo... Pois existe a manipulação de formulário 
+    ];
+
+    protected bool $allowEmptyInserts = false;
+    protected bool $updateOnlyChanged = true;
+
+    protected array $casts = [];
+    protected array $castHandlers = [];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'criado_em';
+    protected $updatedField  = 'atualizado_em';
+    protected $deletedField  = 'detelado_em';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+   
+
+    // Callbacks
+   
+    protected $beforeInsert   = [];   
+    protected $beforeUpdate   = [];
+    
+}
