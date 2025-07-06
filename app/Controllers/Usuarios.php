f9a154029e7805e9111f66758d0ad251e848f354
Gilberto <gilbertonerigodoy@gmail.com>
2025-07-05 21:32:02 -0300

    Alterando o método recupera Usuários

--- app/Controllers/Usuarios.php
+++ app/Controllers/Usuarios.php
@@ -45,9 +45,10 @@ class Usuarios extends BaseController
         $data = [];
 
         foreach($usuarios as $usuario){
+
             $data[] = [
                 'imagem' => $usuario->imagem ? '<img src="' . site_url("recursos/img/usuarios/$usuario->imagem") . '" alt="Imagem de perfil" class="img-fluid" width="50">' : '<img src="' . site_url("recursos/img/usuario_sem_imagem.png") . '" alt="Sem imagem" class="img-fluid" width="50">',
-                'nome'   => esc($usuario->nome),
+                'nome'   => anchor("usuarios/exibir/$usuario->id", esc($usuario->nome), 'title="Exibir usuário '.esc($usuario->nome).'"'),
                 'email'  => esc($usuario->email),
                 'ativo'  => ($usuario->ativo ?  '<i class="fa fa-unlock text-success"></i>&nbsp;Ativo' : '<i class= "fa fa-lock text-warning"></i>&nbsp;Inativo')
             ];
@@ -59,4 +60,31 @@ class Usuarios extends BaseController
 
         return $this->response->setJSON($retorno);
     }
+
+    public function exibir($id = null)
+    {
+        $usuario = $this->buscaUsuarioOu404($id);
+
+        $data = [
+            'titulo' => 'Detalhes do usuário ' . esc($usuario->nome),
+            'usuario' => $usuario,
+        ];
+
+        return view('Usuarios/exibir', $data);
+    }
+
+
+    private function buscaUsuarioOu404(?int $id = null)
+    {
+        if(!$id || !$usuario = $this->usuarioModel->withDeleted(true)->find($id)) 
+        {
+            throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound("Não encontramos o usuário $id");
+            
+        }
+
+        return $usuario;
+        
+    }
+
+
 }
